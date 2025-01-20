3a1b4ad02a0cab606e9470ff0e3900594f5b861e
mylad13 <mfarjadnasab@gmail.com>
2025-01-20 15:23:49 -0500

    edited gazebo_multi_nav2_world.launch.py

--- launch/gazebo_multi_nav2_world.launch.py
+++ launch/gazebo_multi_nav2_world.launch.py
@@ -19,7 +19,9 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, RegisterEventHandler
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, FindExecutable, Command, EnvironmentVariable
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
 from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
@@ -32,15 +34,13 @@ def generate_launch_description():
 
     # Names and poses of the robots
     robots = [
-        {'name': 'tb1', 'x_pose': '-1.5', 'y_pose': '-0.5', 'z_pose': 0.01},
-        {'name': 'tb2', 'x_pose': '-1.5', 'y_pose': '0.5', 'z_pose': 0.01},
-        {'name': 'tb3', 'x_pose': '1.5', 'y_pose': '-0.5', 'z_pose': 0.01},
-        {'name': 'tb4', 'x_pose': '1.5', 'y_pose': '0.5', 'z_pose': 0.01},
+        {'name': 'jc0_0', 'x_pose': '-1.5', 'y_pose': '-0.5', 'z_pose': 0.01},
+        {'name': 'jc0_1', 'x_pose': '-1.5', 'y_pose': '0.5', 'z_pose': 0.01},
+        # {'name': 'tb3', 'x_pose': '1.5', 'y_pose': '-0.5', 'z_pose': 0.01},
+        # {'name': 'tb4', 'x_pose': '1.5', 'y_pose': '0.5', 'z_pose': 0.01},
         # ...
         # ...
         ]
-
-    TURTLEBOT3_MODEL = 'waffle'
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     declare_use_sim_time = DeclareLaunchArgument(
@@ -58,9 +58,9 @@ def generate_launch_description():
     )
 
     
-    turtlebot3_multi_robot = get_package_share_directory('turtlebot3_multi_robot')
+    jackal_multi_robot = get_package_share_directory("jackal_multi_robot")
 
-    package_dir = get_package_share_directory('turtlebot3_multi_robot')
+    package_dir = get_package_share_directory('jackal_multi_robot')
     nav_launch_dir = os.path.join(package_dir, 'launch', 'nav2_bringup')
 
     rviz_config_file = LaunchConfiguration('rviz_config_file')
@@ -70,26 +70,23 @@ def generate_launch_description():
             package_dir, 'rviz', 'multi_nav2_default_view.rviz'),
         description='Full path to the RVIZ config file to use')
 
-    urdf = os.path.join(
-        turtlebot3_multi_robot, 'urdf', 'turtlebot3_' + TURTLEBOT3_MODEL + '.urdf'
-    )
 
     world = os.path.join(
-        get_package_share_directory('turtlebot3_multi_robot'),
+        get_package_share_directory('jackal_multi_robot'),
         'worlds', 'multi_robot_world.world')
-
+    
+    config_jackal_velocity_controller = PathJoinSubstitution(
+            [FindPackageShare('jackal_control'), 'config', 'control.yaml']
+        )
+    
+    # Gazebo server launch file
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')
+            os.path.join(get_package_share_directory("ros_ign_gazebo"), "launch", "ign_gazebo.launch.py")
         ),
-        launch_arguments={'world': world}.items(),
+        launch_arguments={"ign_args": '-r ' + world}.items(),
     )
 
-    gzclient_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzclient.launch.py')
-        ),
-    )
 
     params_file = LaunchConfiguration('nav_params_file')
     declare_params_file_cmd = DeclareLaunchArgument(
@@ -104,15 +101,15 @@ def generate_launch_description():
     ld.add_action(declare_rviz_config_file_cmd)
     ld.add_action(declare_params_file_cmd)
     ld.add_action(gzserver_cmd)
-    ld.add_action(gzclient_cmd)
- 
+
+
     remappings = [('/tf', 'tf'),
                   ('/tf_static', 'tf_static')]
     map_server=Node(package='nav2_map_server',
         executable='map_server',
         name='map_server',
         output='screen',
-        parameters=[{'yaml_filename': os.path.join(get_package_share_directory('turtlebot3_navigation2'), 'map', 'map.yaml'),
+        parameters=[{'yaml_filename': os.path.join(get_package_share_directory('jackal_navigation'), 'map', 'office.yaml'),
                      },],
         remappings=remappings)
 
@@ -138,33 +135,66 @@ def generate_launch_description():
     # Spawn turtlebot3 instances in gazebo
     for robot in robots:
 
+        name = robot['name']
         namespace = [ '/' + robot['name'] ]
+        frame_prefix = [namespace, '/']
+
+        # Get URDF via xacro
+        robot_description_command = [
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
+            PathJoinSubstitution(
+                [FindPackageShare('jackal_description'), 'urdf', 'jackal.urdf.xacro']
+            ),
+            ' ',
+            'is_sim:=true',
+            ' ',
+            'prefix:=',  # Pass the namespace as the prefix argument
+            namespace,
+            ' ',
+            'gazebo_controllers:=',
+            config_jackal_velocity_controller,
+        ]
+
+        robot_description_content = ParameterValue(
+            Command(robot_description_command),
+            value_type=str
+        )
 
         # Create state publisher node for that instance
-        turtlebot_state_publisher = Node(
-            package='robot_state_publisher',
+        jackal_state_publisher = Node(
+            package="robot_state_publisher",
             namespace=namespace,
-            executable='robot_state_publisher',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time,
-                            'publish_frequency': 10.0}],
+            executable="robot_state_publisher",
+            output="screen",
+            parameters=[{"use_sim_time": use_sim_time, # This was set to false in the original implementation, why?
+                            "publish_frequency": 10.0,
+                            'robot_description': robot_description_content,
+                            'frame_prefix': ''.join(frame_prefix)}],
             remappings=remappings,
-            arguments=[urdf],
         )
 
         # Create spawn call
-        spawn_turtlebot3_burger = Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
+        spawn_jackal = Node(
+            package="ros_ign_gazebo",
+            executable="create",
+            namespace=namespace,
             arguments=[
-                '-file', os.path.join(turtlebot3_multi_robot,'models', 'turtlebot3_' + TURTLEBOT3_MODEL, 'model.sdf'),
-                '-entity', robot['name'],
-                '-robot_namespace', namespace,
-                '-x', robot['x_pose'], '-y', robot['y_pose'],
-                '-z', '0.01', '-Y', '0.0',
-                '-unpause',
+                "-topic",
+                f"robot_description",
+                "-name",
+                name,
+                "-x",
+                str(x),
+                "-y",
+                str(y),
+                "-z",
+                "0.51",
+                "-Y",
+                "3.14159",
+                "-unpause",
             ],
-            output='screen',
+            output="screen",
         )
 
         bringup_cmd = IncludeLaunchDescription(
@@ -186,8 +216,8 @@ def generate_launch_description():
 
         if last_action is None:
             # Call add_action directly for the first robot to facilitate chain instantiation via RegisterEventHandler
-            ld.add_action(turtlebot_state_publisher)
-            ld.add_action(spawn_turtlebot3_burger)
+            ld.add_action(jackal_state_publisher)
+            ld.add_action(spawn_jackal)
             ld.add_action(bringup_cmd)
 
         else:
@@ -196,8 +226,8 @@ def generate_launch_description():
             spawn_turtlebot3_event = RegisterEventHandler(
                 event_handler=OnProcessExit(
                     target_action=last_action,
-                    on_exit=[spawn_turtlebot3_burger,
-                            turtlebot_state_publisher,
+                    on_exit=[spawn_jackal,
+                            jackal_state_publisher,
                             bringup_cmd],
                 )
             )
@@ -205,14 +235,31 @@ def generate_launch_description():
             ld.add_action(spawn_turtlebot3_event)
 
         # Save last instance for next RegisterEventHandler
-        last_action = spawn_turtlebot3_burger
+        last_action = spawn_jackal
     ######################
 
     ######################
     # Start rviz nodes and drive nodes after the last robot is spawned
     for robot in robots:
-
+        name = robot['name']
         namespace = [ '/' + robot['name'] ]
+
+        # Get URDF via xacro
+        robot_description_command = [
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
+            PathJoinSubstitution(
+                [FindPackageShare('jackal_description'), 'urdf', 'jackal.urdf.xacro']
+            ),
+            ' ',
+            'is_sim:=true',
+            ' ',
+            'prefix:=',  # Pass the namespace as the prefix argument
+            namespace,
+            ' ',
+            'gazebo_controllers:=',
+            config_jackal_velocity_controller,
+        ]
 
         # Create a initial pose topic publish call
         message = '{header: {frame_id: map}, pose: {pose: {position: {x: ' + \
@@ -235,18 +282,21 @@ def generate_launch_description():
                                    condition=IfCondition(enable_rviz)
                                     )
 
-        drive_turtlebot3_burger = Node(
-            package='turtlebot3_gazebo', executable='turtlebot3_drive',
-            namespace=namespace, output='screen',
-            condition=IfCondition(enable_drive),
-        )
-
+        launch_jackal_control = IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(PathJoinSubstitution(
+                        [FindPackageShare('jackal_control'), 'launch', 'control.launch.py']
+                    )),
+                    launch_arguments=[('robot_description_command', robot_description_command),
+                                    ('is_sim', 'True'),
+                                    ('namespace', namespace)]
+                )
+        
         # Use RegisterEventHandler to ensure next robot rviz launch happens 
         # only after all robots are spawned
         post_spawn_event = RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=last_action,
-                on_exit=[initial_pose_cmd, rviz_cmd, drive_turtlebot3_burger],
+                on_exit=[initial_pose_cmd, rviz_cmd, launch_jackal_control],
             )
         )
 
